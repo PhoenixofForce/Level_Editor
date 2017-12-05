ced6ee2119e497f9155ca6ce1b2b61b55a81c5b0
PhoenixofForce <phoenixofforce@gmail.com>
2017-12-05 20:59:28 +0100

    [+] More Layer [+] Separation Layer/ Layercontroll (Optical) => Todo: Separation in Code

--- src/LayerPane.java
+++ src/LayerPane.java
@@ -1,19 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class LayerPane extends JList {
+public class LayerPane extends JPanel {
 
+	private JList jList;
 	private DefaultListModel listModel;
 
+	private JToolBar toolBar;
+
 	public LayerPane() {
+		this.setLayout(new BorderLayout());
+
 		listModel = new DefaultListModel();
-		this.setModel(listModel);
+		jList = new JList(listModel);
 
-		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		this.setLayoutOrientation(VERTICAL);
+		jList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		jList.setLayoutOrientation(JList.VERTICAL);
 
-		listModel.addElement(new String("Layer 0"));
-		listModel.addElement(new String("Layer 1"));
+		listModel.addElement(new String("Background Layer"));
+		listModel.addElement(new String("Tile Layer"));
+		listModel.addElement(new String("Object Layer"));
+		listModel.addElement(new String("Mob Layer"));
+
+		this.add(jList, BorderLayout.PAGE_START);
+
+		toolBar = new JToolBar();
+		toolBar.add(new JButton("+"));
+		toolBar.add(new JButton("-"));
+		toolBar.setFloatable(false);
+
+		this.add(toolBar, BorderLayout.PAGE_END);
 	}
 
 }
