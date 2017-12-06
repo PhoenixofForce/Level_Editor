faf24c002f4a095b573f6d30172e5b1ae46e9899
PhoenixofForce <phoenixofforce@gmail.com>
2017-12-06 18:19:59 +0100

    [+] Adding, Removing layers => TODO: User can set depth

--- src/window/elements/layer/LayerControll.java
+++ src/window/elements/layer/LayerControll.java
@@ -2,13 +2,36 @@ package window.elements.layer;
 
 import javax.swing.*;
 
-public class LayerControll extends JToolBar {
+public abstract class LayerControll extends JToolBar {
+
+	private JButton add, remove, options;
 
 	public LayerControll() {
-		this.add(new JButton("+"));
-		this.add(new JButton("-"));
+		add = new JButton("+");
+		remove = new JButton("-");
+		//options = new JButton("Options");
+
+		this.add(add);
+		this.add(remove);
+		//this.add(options);
 		this.setFloatable(false);
 		this.setRollover(true);
+
+		this.add.addActionListener(e-> {
+			String s = JOptionPane.showInputDialog(new JFrame(), "Name of the new Layer");
+			Object[] options = new Object[]{"Tilebased", "Pixelbased"};
+			int m = JOptionPane.showOptionDialog(new JFrame(), "Type of the new Layer", "", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+			onAdd(s, m==1);
+		});
+		this.remove.addActionListener(e -> onRemove());
+	}
+
+	public void onAdd(String name, boolean type) {
+
+	}
+
+	public void onRemove() {
+
 	}
 
 }
--- src/window/elements/layer/LayerPane.java
+++ src/window/elements/layer/LayerPane.java
@@ -1,16 +1,28 @@
 package window.elements.layer;
 
+import data.Layer;
+import data.TileLayer;
+
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.*;
 import java.awt.*;
 
 public class LayerPane extends JPanel {
 
+	private LayerPane INSTANCE;
+
 	private JList jList;
 	private DefaultListModel listModel;
-
 	private LayerControll lc;
 
+	private Map<String, Layer> layers;
+
 	public LayerPane() {
+		layers = new HashMap<>();
+
+		INSTANCE = this;
+
 		this.setLayout(new BorderLayout());
 
 		listModel = new DefaultListModel();
@@ -19,15 +31,52 @@ public class LayerPane extends JPanel {
 		jList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		jList.setLayoutOrientation(JList.VERTICAL);
 
-		listModel.addElement(new String("Background layer"));
-		listModel.addElement(new String("Tile layer"));
-		listModel.addElement(new String("Object layer"));
-		listModel.addElement(new String("Mob layer"));
+		add("Background", true);
+		add("Tile", false);
+		add("Object", true);
+
+		jList.setSelectedIndex(0);
 
 		this.add(jList, BorderLayout.PAGE_START);
 
-		lc = new LayerControll();
+		lc = new LayerControll() {
+			@Override
+			public void onAdd(String name, boolean type) {
+				INSTANCE.add(name, type);
+			}
+
+			@Override
+			public void onRemove() {
+				if(jList.getSelectedIndex() < 0) return;
+				int sel = jList.getSelectedIndex();
+				String name = (String)listModel.get(sel);
+				layers.remove(name);
+				listModel.remove(sel);
+				if(sel == 0) jList.setSelectedIndex(0);
+				else jList.setSelectedIndex(sel-1);
+			}
+		};
 		this.add(lc, BorderLayout.PAGE_END);
+	}
+
+	private void add(String name, boolean type) {
+		if(layers.keySet().contains(name)) {
+			add(name + "(" + 1 + ")", type, 1);
+			return;
+		}
+		layers.put(name, type? null: new TileLayer(10, 10, 0));
+		listModel.addElement(name);
+		jList.setSelectedIndex(listModel.indexOf(name));
+	}
+
+	private void add(String name, boolean type, int rek) {
+		if(layers.keySet().contains(name)) {
+			add(name.split("\\(")[0] + "(" + 1 + ")", type, rek+1);
+			return;
+		}
+		layers.put(name, type? null: new TileLayer(10, 10, 0));
+		listModel.addElement(name);
+		jList.setSelectedIndex(listModel.indexOf(name));
 	}
 
 }
